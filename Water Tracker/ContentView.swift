523c57878a0fdda7cce53d64aba589786122d75d
Pavel Markitantov <markitantow@gmail.com>
2024-01-15 13:03:46 +0100

    Initial Commit

---
+++ Water Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water Tracker
+//
+//  Created by Pavel Markitantov on 15/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water Tracker/Water_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_TrackerApp.swift
+//  Water Tracker
+//
+//  Created by Pavel Markitantov on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Water_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
